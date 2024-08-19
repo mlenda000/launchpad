9ef4719633f9f81e0343d186c2565da99acc6d8f
Lendacky, Mark <Mark_Lendacky@cable.comcast.com>
2024-08-19 15:51:02 -0400

    started adding data

---
+++ launchpad/src/Data/Data.js
@@ -0,0 +1,86 @@
+export const streamingData = [
+  {
+    id: "apple",
+    display: "Apple TV+",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: false,
+  },
+  {
+    id: "netflix",
+    display: "Netflix",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "hulu",
+    display: "Hulu",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "disney",
+    display: "Disney+",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: false,
+  },
+  {
+    id: "max",
+    display: "HBO Max",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "peacock",
+    display: "Peacock",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "amazon",
+    display: "Amazon Prime Video",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "youtube",
+    display: "YouTube Premium",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: false,
+  },
+  {
+    id: "crunchyroll",
+    display: "Crunchyroll",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+  {
+    id: "hidive",
+    display: "HIDIVE",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: false,
+  },
+  {
+    id: "paramount",
+    display: "Paramount+",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: false,
+  },
+  {
+    id: "xfinity",
+    display: "Xfinity",
+    imgLink:
+      "https://www.apple.com/v/apple-tv-plus/a/images/overview/hero__dvsxv8smru6u_large.jpg",
+    subscribed: true,
+  },
+];
