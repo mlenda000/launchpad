88edeb1122de590aa77b3969b1ade0b708c72b60
Mark <Mark_Lendacky@cable.comcast.com>
2024-08-26 14:14:12 -0400

    broken logo img fix

--- launchpad/src/Components/Logo/Logo.js
+++ launchpad/src/Components/Logo/Logo.js
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <div className="logo-layout">
       <img
-        src={process.env.PUBLIC_URL + "launchpad-logo.png"}
+        src="launchpad-logo.png"
         alt="launchpad logo"
         width="150px"
         height="150px"
